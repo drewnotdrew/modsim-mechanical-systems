6c3defc1d65da994a3e347b104b65bf85ac543ea
supah32 <95254830+supah32@users.noreply.github.com>
2021-12-12 14:19:15 -0500

    Integration of Code into Essay
    Integrated the code from the draft section into the Comp Essay, and cleaned up our code a little to make it smoother. Also added work from google doc to the comp essay.

--- body_2D_ode45.m
+++ body_2D_ode45.m
@@ -8,27 +8,18 @@ bodyVelocity = [launchXvelocity,launchYvelocity]; % (m/s)
 % Model parameters
 G = 6.67e-11; % Gravitational Constant ((N*m^2)/kg^2)
 earthMass = 5.97e24; % (kg)
-sunMass = 1.99e30; % (kg)
 moonMass = 7.348e22; % Mass of the Moon (kg)
 bodyMass = 60; % Mass of the body being fired out of our cannon (kg)
 d = 3.844e8; % Distance between the Earth and the Moon (m)
 rEarth = 6.371e6; % Radius of Earth
-rMoon = 1.738e6; % Radius of Earth
 
 % Initial parameters 
-earthX = 0;
-earthY = 0;
-earthPos = [earthX, earthY];
-moonX = d;
-moonY = 0;
-moonPos = [moonX, moonY];
 bodyX = 0;
 bodyY = rEarth; 
 bodyPos = [bodyX, bodyY];
-% bodyMoonPos = [-d, rEarth];
 bodyParameters = [bodyPos, bodyVelocity]; % bodyMoonPos may be needed (?)
-% Simulation parameters
 
+% Simulation parameters
 t_0 = 0;
 t_end = 60*60*24*7;
 t_span = [t_0, t_end];
@@ -38,13 +29,10 @@ options = odeset('Events',@event_func, 'RelTol', 1e-5);
 [T, M] = ode45(@rate_func, t_span, bodyParameters, options);
     function res = rate_func(~,M)
         bodyPosition = M(1:2);
-%         bodyMoonPosition = M(3:4);
         bodyVelocity = M(3:4);
         bodyMoonPosition = bodyPosition - [-d; rEarth];
-        dPdt = [bodyVelocity]; % [bodyMoonPosition]
+        dPdt = [bodyVelocity];
         dVdt = acceleration(bodyPosition, bodyMoonPosition, bodyVelocity);
-       L  = size(dPdt);
-       L2 = size(dVdt);
         res = [dPdt; dVdt];
     end
     
@@ -52,15 +40,13 @@ options = odeset('Events',@event_func, 'RelTol', 1e-5);
         gravForceEarth = -(G*earthMass*bodyMass)/(norm(bodyPosition)^2) * (bodyPosition/norm(bodyPosition));
         gravForceMoon = -(G*moonMass*bodyMass)/(norm(bodyMoonPosition)^2) * (bodyMoonPosition/norm(bodyMoonPosition));
         dAdt = gravForceEarth/bodyMass + gravForceMoon/bodyMass;
-        moonbrr = size(gravForceMoon);
-        earthbrr = size(gravForceEarth);
-        jerkbrr = size(dAdt);
         res = [dAdt];
 
     end
 end
+    % Helper function
     function [value, isterminal, direction] = event_func(T, M)
-        P = M(1:2); %Position of body
+        P = M(1:2); % Position of body
         P_1 = M(1:2)-[3.844e8; 0]; % Tried to use moonX and moonY. How do we use these in the event function?
         dist = norm(P_1) - 1.738e6 - 75000*1000;
         value = sign(dist) + 1;
